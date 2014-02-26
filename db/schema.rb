68073d88a04fa5cc0f8cc141ed47da7d857b88cb
Andrey Krahmal <coolrighthere@yahoo.com>
2014-02-26 14:13:46 +0000

    User logs in and signs up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225185815) do
+ActiveRecord::Schema.define(version: 20140226114438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20140225185815) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_confirmation"
   end
 
 end
